62d4c85b41798c0ab924c0bdf303d89265b352c6
王春杉 <562932199@qq.com>
2020-12-01 20:40:16 +0800

    添加了一般员工页面 跳转成功

--- back-end/src/main/java/com/shan/askforleave/controller/AdminPageController.java
+++ back-end/src/main/java/com/shan/askforleave/controller/AdminPageController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminPageController {
 
+    // 登录页面
     @RequestMapping("/login_page")
     public String login() {
         return "login";
     }
 
+    // 一般员工页面
+    @RequestMapping("/employee_page")
+    public String employee() {
+        return "employeePage";
+    }
 
 }
