8a51f6a1a6e60c70e417f16cc2ab39ab9e10e060
王春杉 <562932199@qq.com>
2020-12-01 18:03:57 +0800

    合并了前后端 调试好久 终于调好了接口

--- back-end/src/main/java/com/shan/askforleave/controller/AdminPageController.java
+++ back-end/src/main/java/com/shan/askforleave/controller/AdminPageController.java
@@ -1,10 +1,17 @@
 package com.shan.askforleave.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminPageController {
+
+    @RequestMapping("/login_page")
+    public String login() {
+        return "login";
+    }
 
 
 }
--- back-end/src/main/java/com/shan/askforleave/controller/UserController.java
+++ back-end/src/main/java/com/shan/askforleave/controller/UserController.java
@@ -5,6 +5,7 @@ import com.shan.askforleave.service.UserService;
 import com.shan.askforleave.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +19,7 @@ public class UserController {
     UserService userService;
 
 
-    @RequestMapping("login")
+    @PostMapping("login")
     @ResponseBody
     public Object login(@RequestParam("username") String username, @RequestParam("password") String password,
                         @RequestParam("role") int loginRole) {
